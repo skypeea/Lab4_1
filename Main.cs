cb7dcec5d737bfd1013bbd39730f7fa97c9b1f2a
Алексей <lebedevalexeyo@gmail.com>
2023-01-21 18:35:03 +1100

    /

---
+++ Main.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var wallList = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .WhereElementIsNotElementType()
+                .Cast<Wall>()
+                .ToList();
+            string wallInfo = String.Empty;
+
+            foreach (var wall in wallList)
+            {
+                double wallVolume = wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble();
+                wallVolume = UnitUtils.ConvertFromInternalUnits(wallVolume, UnitTypeId.CubicMeters);
+                wallInfo += $" {wall.Name} \t {wallVolume.ToString()} {Environment.NewLine}";
+            }
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string csvPath = Path.Combine(desktopPath, "WallInfo.csv");
+            File.WriteAllText(csvPath, wallInfo);
+            return Result.Succeeded;
+
+        }
+    }
+}
